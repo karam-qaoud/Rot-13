8913292f7d6da8004857332af89d018e8480f323
Karam Qaoud <karamqaoud@gmail.com>
2021-03-04 12:33:37 +0100

    cleaning code

--- src/rot-13.js
+++ src/rot-13.js
@@ -1,23 +1,23 @@
 console.log("Rot-13 algorithm ...");
 
 const convert = (text) => {
-  const smallAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  const capitalAlphabet = smallAlphabet.map((letter) => letter.toUpperCase());
+  const lCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  const uCase = lCase.map((letter) => letter.toUpperCase());
   let textArr = text.split("");
   let replace = (arr, m) => {
     let index = arr.indexOf(textArr[m]);
     if (index >= 13) {
-      index = index - 26 + 13; //English alphabet is 26 letters
+      index = index - 13;
       textArr[m] = arr[index];
     } else {
       textArr[m] = arr[index + 13];
     }
   };
   for (let i = 0; i < textArr.length; i++) {
-    if (smallAlphabet.includes(textArr[i])) {
-      replace(smallAlphabet, i);
-    } else if (capitalAlphabet.includes(textArr[i])) {
-      replace(capitalAlphabet, i);
+    if (lCase.includes(textArr[i])) {
+      replace(lCase, i);
+    } else if (uCase.includes(textArr[i])) {
+      replace(uCase, i);
     }
   }
   text = textArr.join("");
